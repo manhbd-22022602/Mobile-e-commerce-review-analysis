44743e633661f6511a754d36c11331f2229b47ff
h22022522 <127271950+h22022522@users.noreply.github.com>
2024-03-14 00:48:43 +0700

    update file comment.csv test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Edge()
+
+driver.get("https://www.thegioididong.com/dtdd")
+
+
+# Tìm phần tử bằng XPath
+product = driver.find_element(By.XPATH, '//*[@id="categoryPage"]/div[3]/ul/li[1]/a[1]')
+product.click()
+
+comment = driver.find_element(By.XPATH, '/html/body/section[1]/div[3]/div[1]/div[18]/div[2]/div/div/div[6]/div/a')
+comment.click()
+
+comment_detail = driver.find_element(By.XPATH, '//*[@id="r-56509474"]/div[3]/p')
+print(comment_detail.text)
